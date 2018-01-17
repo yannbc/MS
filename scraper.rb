81c17dafc7204fbadf696c08dc7bfc46c070ba15
yBC <yann@billcap.com>
2018-01-17 17:37:26 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,19 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+ require 'scraperwiki'
+ require 'mechanize'
 #
-# agent = Mechanize.new
+ agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+ page = agent.get("https://www.multisportaustralia.com.au/m3/event?c=1&r=6335&e=1")
 #
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+ p page.at('/html/body/section[2]/div/div[3]/div')
 #
 # # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+ ScraperWiki.save_sqlite(["name"], {"name" => "yann", "occupation" => "software developer"})
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
